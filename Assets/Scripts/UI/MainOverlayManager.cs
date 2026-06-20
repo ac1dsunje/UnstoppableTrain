c0ef497190ac2764d46ca21de69163932769a293
ac1dsunje <ar.cherba63programming@gmail.com>
2026-06-20 02:44:42 +0200

    added general passengers info to overlay

--- Assets/Scripts/UI/MainOverlayManager.cs
+++ Assets/Scripts/UI/MainOverlayManager.cs
@@ -1,9 +1,15 @@
-﻿using TMPro;
+﻿using System;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class MainOverlayManager : ScreenManager
 {
     [SerializeField] private TextMeshProUGUI _chunksPassedText;
+    
+    [SerializeField] private TextMeshProUGUI _rolesText;
+    [SerializeField] private TextMeshProUGUI _traitsText;
+
     [SerializeField] private TrainController _train;
 
     private void OnEnable()
@@ -29,5 +35,38 @@ public class MainOverlayManager : ScreenManager
     private void UpdateStats(TrainStats stats)
     {
         _chunksPassedText.text = $"chunks passed: {stats.chunksPassed}";
+
+        _rolesText.text = GetRolesInfo(stats);
+        _traitsText.text = GetTraitsInfo(stats);
+    }
+
+    private string GetRolesInfo(TrainStats stats)
+    {
+        if (stats._passengers == null || stats._passengers.Count == 0)
+            return " ";
+
+        string info = "";
+        foreach (Role role in Enum.GetValues(typeof(Role)))
+        {
+            int count = stats._passengers.Count(p => p.role == role);
+            info += $"{role}: {count}\n";
+        }
+
+        return info.TrimEnd('\n');
+    }
+
+    private string GetTraitsInfo(TrainStats stats)
+    {
+        if (stats._passengers == null || stats._passengers.Count == 0)
+            return " ";
+
+        string info = "";
+        foreach (Trait trait in Enum.GetValues(typeof(Trait)))
+        {
+            int count = stats._passengers.Count(p => p.trait == trait);
+            info += $"{trait}: {count}\n";
+        }
+
+        return info.TrimEnd('\n');
     }
 }
