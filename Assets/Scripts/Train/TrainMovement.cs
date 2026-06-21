a45a350f2f7a66c64099985cb4b233bff63c2391
ac1dsunje <ar.cherba63programming@gmail.com>
2026-06-21 20:26:04 +0200

    added switch rails speed (no teleport anymore)

--- Assets/Scripts/Train/TrainMovement.cs
+++ Assets/Scripts/Train/TrainMovement.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 public class TrainMovement : MonoBehaviour
 {
     [SerializeField] private GameManager _gameManager;
+    [SerializeField] private float _switchRailsSpeed = 5f;
     private Imovement _controller;
     private Rigidbody _rigidBody;
     private RoadController _currentRoad;
+
+    private bool _isMovingToSide = false;
+    private Vector3 _targetPos = new();
 
     private void OnEnable()
     {
@@ -32,8 +36,15 @@ public class TrainMovement : MonoBehaviour
         MoveForward();
     }
 
+    private void Update()
+    {
+        MoveToSide(_targetPos);
+    }
+
     private void MoveForward()
     {
+        if (_isMovingToSide) return;
+
         _rigidBody.linearVelocity = transform.forward * _controller.GetSpeed();
     }
 
@@ -42,8 +53,8 @@ public class TrainMovement : MonoBehaviour
         _currentRoad = _controller.GetCurrentRoad();
         if (_currentRoad.IsLeftActive) return;
 
-        transform.position = new Vector3(_currentRoad.LeftRail.transform.position.x, transform.position.y, transform.position.z);
-        // some animations?
+        _targetPos = new Vector3(_currentRoad.LeftRail.transform.position.x, transform.position.y, transform.position.z);
+        _isMovingToSide = true;
     }
 
     private void MoveRight()
@@ -51,7 +62,19 @@ public class TrainMovement : MonoBehaviour
         _currentRoad = _controller.GetCurrentRoad();
         if (_currentRoad.IsRightActive) return;
 
-        transform.position = new Vector3(_currentRoad.RightRail.transform.position.x, transform.position.y, transform.position.z);
-        // some animations?
+        _targetPos = new Vector3(_currentRoad.RightRail.transform.position.x, transform.position.y, transform.position.z);
+        _isMovingToSide = true;
+    }
+
+    private void MoveToSide(Vector3 targetPos)
+    {
+        if (_isMovingToSide)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, _switchRailsSpeed * Time.deltaTime);
+        }
+        if (transform.position == targetPos)
+        {
+            _isMovingToSide = false;
+        }
     }
 }
